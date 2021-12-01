d6753d401abbcbcd5e7c84a469e5cf1d5be130d0
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2021-12-01 12:08:14 +0100

    feat: mongoose

--- backend/src/api/images.js
+++ backend/src/api/images.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const router = express.Router();
+const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost:27017/test';
 
+const imageSchema = new mongoose.Schema({
+  title: String,
+  size: String,
+  source: String
+});
+
+mongoose.connect(databaseUrl);
 router.get('/', (req, res) => {
-  res.json([
-    {
-      title: 'IMG_4985.HEIC',
-      size: '3.9 MB',
-      source:
-        'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-    }
-  ]);
+  imageSchema.find().then((data) => {
+    console.log(data);
+    res.json([
+      {
+        title: 'IMG_4985.HEIC',
+        size: '3.9 MB',
+        source:
+          'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+      }
+    ]);
+  });
 });
 
 module.exports = router;
