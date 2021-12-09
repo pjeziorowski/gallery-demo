3984a7f67d313a5219e9a4f89844c1256088d4a3
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2021-12-09 14:34:42 +0100

    fix: index name

--- backend/src/api/images.js
+++ backend/src/api/images.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const router = express.Router();
-const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost:27017/test';
+const databaseUrl = process.env.DATABASE_URL
+    || 'mongodb://localhost:27017/test';
 
 const imageSchema = new mongoose.Schema({
   title: String,
@@ -10,7 +11,9 @@ const imageSchema = new mongoose.Schema({
   source: String
 });
 
-mongoose.connect(databaseUrl);
+console.log('Connecting' + databaseUrl)
+
+// mongoose.connect(databaseUrl);
 router.get('/', (req, res) => {
   imageSchema.find().then((data) => {
     console.log(data);
@@ -19,7 +22,7 @@ router.get('/', (req, res) => {
         title: 'IMG_4985.HEIC',
         size: '3.9 MB',
         source:
-          'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+            'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
       }
     ]);
   });
